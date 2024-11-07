1a30e33782e1add4c5ffbad8d274c594b7fbf7f3
Tanmay337442 <tanmay337442@gmail.com>
2024-11-07 11:35:06 -0500

    initial commit

---
+++ scripts/footer.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $("#footer-placeholder").load("/site/footer.html");
+});
---
+++ scripts/nav.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $("#nav-placeholder").load("/site/nav.html");
+});
